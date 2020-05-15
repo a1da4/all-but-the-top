6b3b3c029ad1d977b8fba16f888332d6990871f3
a1da4 <ai.the.taichi@gmail.com>
2020-05-16 01:23:34 +0900

    adopt gensim word2vec

--- vector_postprocess.py
+++ vector_postprocess.py
@@ -11,19 +11,34 @@ def main(args):
     
     :return: postprocessed vector
     """
-    wv = np.load(args.model_path)
+    if args.is_word2vec:
+        # gensim.models Word2Vec model
+        from gensim.models import Word2Vec
+        model = Word2Vec.load(args.model_path)
+        wv = model.wv.syn0
+    else:
+        # numpy model
+        wv = np.load(args.model_path)
+
+    print(args.is_word2vec)
+
     pca = PCA(n_components=args.n_components)
     mean = np.average(wv, axis=0)
     pca.fit(wv - mean)
     components = np.matmul(np.matmul(wv, pca.components_.T), pca.components_)
     processed = wv - mean - components
-    np.save(f'WV_abtt-{args.n_components}', processed)
+    if args.is_word2vec:
+        model.wv.syn0 = processed
+        model.save(f'{args.model_path}_abtt-{args.n_components}')
+    else:
+        np.save(f'{args.model_path[:-4]}_abtt-{args.n_components}.npy', processed)
 
 
 def cli_main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', help='path of model')
-    parser.add_argument('--n_components', type=int, default=5, help='number of dimensions postprocessing')
+    parser.add_argument('-m', '--model_path', help='path of model')
+    parser.add_argument('-n', '--n_components', type=int, default=5, help='number of dimensions postprocessing')
+    parser.add_argument('-w', '--is_word2vec', action='store_true', help='if the model is word2vec in gensim, please call')
 
     args = parser.parse_args()
     main(args)
